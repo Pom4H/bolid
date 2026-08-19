02df6ad526833f68c3dd7e24a11a0421692a12e6
Roman Popov <atfpom4h@gmail.com>
2026-08-19 19:33:32 +0300

    test: cover current UUID and name discovery

--- mobile/core/src/commonTest/kotlin/ru/bolid/testdpls/core/app/DplsBleTest.kt
+++ mobile/core/src/commonTest/kotlin/ru/bolid/testdpls/core/app/DplsBleTest.kt
@@ -16,50 +16,36 @@ class DplsBleTest {
     }
 
     @Test
-    fun discoveredParsesManufacturerPayloadOnce() {
-        val androidScan = DplsBle.discovered(
-            address = "AA:BB",
-            advertisedName = null,
-            peripheralName = null,
-            manufacturerPayload = byteArrayOf(0x31, 0x3B, 0x00, 0x00, 0x05),
-            manufacturerIncludesCompanyId = false,
-            rssi = -51,
-        )
-        assertEquals("AA:BB", androidScan.address)
-        assertEquals("Test-DPLS-3B31", androidScan.name)
-        assertEquals(0x3B31, androidScan.deviceId)
-        assertEquals(5, androidScan.advStatus)
-        assertEquals(-51, androidScan.rssi)
-        assertEquals(null, androidScan.firmwareVersion)
-
-        val withFw = DplsBle.discovered(
+    fun discoveredUsesCurrentAirNameOnly() {
+        val named = DplsBle.discovered(
             address = "AA:BB",
             advertisedName = "Test-DPLS-3B31",
             peripheralName = null,
-            manufacturerPayload = byteArrayOf(0x31, 0x3B, 0x00, 0x00, 0x00, 1, 4, 0),
-            manufacturerIncludesCompanyId = false,
-            rssi = -40,
+            rssi = -51,
         )
-        assertEquals("1.4.0", withFw.firmwareVersion)
+        assertEquals("AA:BB", named.address)
+        assertEquals("Test-DPLS-3B31", named.name)
+        assertEquals(0x3B31, named.deviceId)
+        assertEquals(0, named.advStatus)
+        assertEquals(-51, named.rssi)
+        assertNull(named.firmwareVersion)
 
-        val iosScan = DplsBle.discovered(
-            address = "uuid",
-            advertisedName = "Live",
-            peripheralName = "Other",
-            manufacturerPayload = byteArrayOf(0x01, 0x0B, 0x31, 0x3B, 0x00, 0x00),
-            manufacturerIncludesCompanyId = true,
-            rssi = -40,
+        val compact = DplsBle.discovered(
+            address = "mac",
+            advertisedName = "DPLS1234",
+            peripheralName = null,
+            rssi = -67,
         )
-        assertEquals("Live", iosScan.name)
-        assertEquals(0x3B31, iosScan.deviceId)
-        assertEquals(0, iosScan.advStatus)
+        assertEquals(0x1234, compact.deviceId)
+        assertEquals("Test-DPLS-1234", compact.name)
 
-        val empty = DplsBle.discovered("x", null, null, null, false, 0)
+        val empty = DplsBle.discovered(
+            address = "x",
+            advertisedName = null,
+            peripheralName = null,
+            rssi = 0,
+        )
         assertNull(empty.deviceId)
         assertEquals("Test-DPLS", empty.name)
-
-        val macAir = DplsBle.discovered("mac", "DPLS1234", null, null, false, -67)
-        assertEquals(0x1234, macAir.deviceId)
-        assertEquals("Test-DPLS-1234", macAir.name)
     }
 }
