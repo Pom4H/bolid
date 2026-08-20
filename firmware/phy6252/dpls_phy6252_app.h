8e1e599713912764bc8b32233da42eae199615cd
Roman Popov <atfpom4h@gmail.com>
2026-08-20 18:05:27 +0300

    refactor(rc5): add disconnected storage event

--- firmware/phy6252/dpls_phy6252_app.h
+++ firmware/phy6252/dpls_phy6252_app.h
@@ -6,6 +6,7 @@
 #define DPLS_PHY6252_RX_EVT 0x0040
 #define DPLS_PHY6252_TX_EVT 0x0400
 #define DPLS_PHY6252_ADC_EVT 0x0800
+#define DPLS_PHY6252_STORAGE_EVT 0x1000
 
 void dpls_phy6252_init(uint8 task_id);
 void dpls_phy6252_connected(uint16 conn_handle);
@@ -16,6 +17,9 @@ void dpls_phy6252_process_rx(void);
  * window averaging live, off the interrupt path. */
 void dpls_phy6252_process_adc(void);
 void dpls_phy6252_process_tx(void);
+/* Flush at most one deferred journal block. This event is armed only after the
+ * BLE link is down, so SNV erase/write never competes with a connection event. */
+void dpls_phy6252_process_storage(void);
 void dpls_phy6252_tx_confirmed(void);
 void dpls_phy6252_tick(void);
 uint32 dpls_phy6252_led_tick(void);
